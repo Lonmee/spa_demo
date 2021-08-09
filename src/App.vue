e92c909273516e8663a8298873d46af190a64089
Lonmee <Lonmee@126.com>
2021-08-10 00:33:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <router-view></router-view>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script setup>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+nav {
+  text-align: center;
+}
+
+nav a {
+  margin: 0 10px;
+}
+</style>
