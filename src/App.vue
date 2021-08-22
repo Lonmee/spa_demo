af03a02683eec0fdfa51f28a8c211ed74e6f2eea
Lonmee <Lonmee@126.com>
2021-08-22 17:07:56 +0800

    xx

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@ import BtmNav from "./views/shared/BtmNav.vue";
 </script>
 
 <template>
-  <router-view class="flex-1 content" @dblclick.prevent/>
+  <router-view class="flex-1 content" @dblclick.prevent #="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
   <btm-nav/>
 </template>
 
