8a77eb1033c26889fc6b6cc0c25af311285d1506
Lonmee <Lonmee@126.com>
2021-10-05 15:29:13 +0800

    h2

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 
+/**
+ * LOCAL_PROXY: same site with 'api' proxy
+ * LOCAL: same site without proxy (CROS)
+ * REMOTE_PROXY: remote with 'api' proxy
+ * @type {{LOCAL_PROXY: string, LOCAL: string, REMOTE_PROXY: string}}
+ */
 const host = {
-    PROXY: '/api',
+    LOCAL_PROXY: '/api',
     LOCAL: 'https://localhost:8081',
-    REMOTE: 'https://localhost/api',
+    REMOTE_PROXY: 'https://localhost/api',
 }
-axios.defaults.baseURL = host.LOCAL;
+axios.defaults.baseURL = import.meta.env.DEV ? host.LOCAL : host.LOCAL_PROXY;
 // 默认情况下，跨源请求不提供凭据(cookie、HTTP认证及客户端SSL证明等)。
-axios.defaults.withCredentials = axios.defaults.baseURL !== host.PROXY;
+axios.defaults.withCredentials = true;
 // axios.defaults.auth = {
 //     username: '',
 //     password: '',
 // };
-
 export const API = {
     USERS_V1: '/1/users',
     SIGN: '/1/log',
 }
-
